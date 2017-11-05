71d7e16e8a5f52fd38b93f66b7b1be328a86f829
Raphael Ohanian VM <raphael.ohanian@telecomnancy.net>
2017-11-05 14:20:29 +0100

    Début projet

--- Makefile
+++ Makefile
@@ -1,4 +1,26 @@
-CC=gcc
-CFLAGS=-Wall
+# Dans le bash, au choix:
+# make all : nettoie le dossier puis compile
+# make project : compile le projet
+# make mrpropre : supprime les *.o
 
-all: tesh
+
+CC = gcc
+CFLAGS = -Wall -Wextra
+LDFLAGS = -lSDL -lSDLmain -lSDL_mixer
+EXEC = project
+
+all : clean $(EXEC)
+	./$(EXEC)
+
+$(EXEC) : debut.o cut_buffer.o
+	$(CC) -o $@ $^ $(LDFLAGS)
+
+%.o : %.c
+	$(CC) -o $@ -c $^ $(CFLAGS)
+
+clean:
+	rm -rf project *.o
+	echo Clean done
+
+mrpropre:
+	rm -rf *.o
---
+++ cut_buffer.c
@@ -0,0 +1,54 @@
+#include "all.h"
+
+void cut_buffer()
+{
+    // Servira à stocker chaque mot de la saisie utilisateur
+    char oneword[20];
+    // On stocke la saisie utilisateur
+    char *token = buffer;
+    // Index des différents tableau
+    int i = 0, i2 = 0, i3 = 0;
+ 
+    for(i=0;i<strlen(token);i++)
+    {
+        switch(token[i])
+        {
+            case ' ':
+                {
+                    // On supprimme les espaces
+                    i=i+1;
+                    every_word[i2] = oneword;
+                    i2=i2+1;
+                };
+                break;
+ 
+            case '\0':
+                {
+                    // On supprimme les \0
+                    i=i+1;
+                    every_word[i2] = oneword;
+                    i2=i2+1;
+                };
+                break;
+ 
+            case '\n':
+                {
+                    // On supprimme les \n
+                    i=i+1;
+                    every_word[i2] = oneword;
+                    i2=i2+1;
+                };
+                break;
+
+            default :
+                {
+                    // On constitue le mot au complet
+                    oneword[i3] = token[i];
+                    i3=i3+1;
+                    oneword[i3] = '\0';
+                };
+                break;
+        }
+    }
+    every_word[i2] = NULL;
+}
