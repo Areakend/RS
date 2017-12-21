1ed9310c2f72c7029d6dacba978b2f44bca72ebf
Thibault HENRY <thibault.henry@telecomnancy.eu>
2017-12-21 09:00:31 +0100

    Grosse avancée, ça marche

--- all.h
+++ all.h
@@ -1,41 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <errno.h>
-#include <signal.h>
 
 #ifndef ALL_H
 #define ALL_H
 #define MAX_ARGS 16
-#define MAX_ARGS_SIZE 64
 #define MAX_INPUT_SIZE 256
 #define MAX_PIPES 32
-#define STDIN_FILENO 0
-#define STDOUT_FILENO 1
+#define MAX_SEMICOLON 32
 
-// Variable Globale
+// --------- Globals
 char buffer[MAX_INPUT_SIZE];
-char* every_word[64];
-char* commands[MAX_PIPES][MAX_ARGS];
-int indexOfPipe[MAX_PIPES];
-int static child = 0;
+char* lines[MAX_SEMICOLON];
+char* commands[MAX_SEMICOLON][MAX_PIPES][MAX_ARGS];
 
+int indexOfSemicolon[MAX_SEMICOLON];
+int indexOfPipe[MAX_SEMICOLON][MAX_PIPES];
+int nbrOfLines; //Define by number of semicolon + 1;
+int nbrOfCommands[MAX_SEMICOLON]; //Define by number of pipes in line + 1;
+// --------- Globals
+
+// --------- Prototypes
+void splitBySemicolon(char *buffer);
 void inputParser(char* buffer);
 int preprocessing(char *buffer);
+char* copyBuffer(char *buffer);
+void displayUserPath();
+void clearTab();
+
 void execCD();
 void execPWD();
 void execRedirect(char *fichier);
-void clearTab();
-char *commandAfterV();
-void execPtVirgule();
-int checkPipe(char *buffer);
-void execPipe(int pos, int in_fd);
-void runCommand(const char *first, char *const argv[]);
+void execSemicolon(int commandIndex); //Précédement execPtVirgule
+void execPipe(int line);
+void execWithPriority(int commandIndex);
 
+int checkSemicolon(char *buffer); //Précédement commandAfterV
+int checkPipe(char *buffer, int line);
 
-void displayUserPath();
+void run(int line);
+// --------- Prototypes
 
 
 #endif
--- tesh.c
+++ tesh.c
@@ -1,42 +1,79 @@
 #include "all.h"
 
-void WaitProcess(pid_t pid)
-{
-    while(1)
-    {
-        int status;
-        int r = waitpid(pid,&status,0);
+void WaitProcess(pid_t pid) {
+  while(1)
+  {
+      int status;
+      int r = waitpid(pid,&status,0);
+      if(r < 0) {
+        if(errno == EINTR)
+          continue;
+        else
+          break;
+      }
+      break;
+  }
+}
 
-        if(r < 0)
-        {
-            if(errno == EINTR)
-                continue;
-            else
-                break;
-        }
-        break;
+int checkSemicolon(char *buffer) {
+  int i = 0;
+  int nbrOfSemicolon = 0;
+  char* token;
+
+  token = strtok(buffer, " ");
+  while(token != NULL) {
+    if (strcmp(token,";") == 0) {
+      indexOfSemicolon[nbrOfSemicolon] = i;
+      nbrOfSemicolon++;
     }
+    token = strtok(NULL, " ");
+    i++;
+  }
+  nbrOfLines = nbrOfSemicolon + 1;
+  return nbrOfSemicolon;
 }
 
-char *commandAfterV() {
-	int i=0;
-	char *inFile=NULL;
-	while (every_word[i]!=NULL) {
-		if (strcmp(every_word[i],";")==0) {
-			inFile = every_word[i+1];
-			while (every_word[i+2]!=NULL) {
-				every_word[i] = every_word[i+2];
-				i++;
-			}
-			every_word[i]=NULL;
-			return(inFile);
-		}
-		i++;
-	}
-	return(inFile);
+void splitBySemicolon(char *buffer) {
+  char *line = strtok(buffer, ";");
+  int i = 0; int offset = 0;
+
+  while(line != NULL) {
+    offset = preprocessing(copyBuffer(line));
+    lines[i] = malloc(sizeof(line + offset));
+    strcpy(lines[i], line + offset);
+    line = strtok(NULL, ";");
+    i++;
+  }
 }
 
-char *commandAfterA() {
+int checkPipe(char *buffer, int line) {
+  int i = 0;
+  int nbrOfPipe = 0;
+  char* token;
+
+  token = strtok(buffer, " ");
+  while(token != NULL) {
+    if (strcmp(token,"|") == 0) {
+      indexOfPipe[line][nbrOfPipe] = i;
+      nbrOfPipe++;
+    }
+    token = strtok(NULL, " ");
+    i++;
+  }
+  nbrOfCommands[line] = nbrOfPipe + 1;
+  return nbrOfPipe;
+}
+
+/*
+void execSemicolon() {
+  char *cmd2 = strok(every_word, ";")
+  while (cmd!=NULL) {
+    execprgm(cmd2[0], cmd2);
+    cmd2= strtok(NULL, ";")
+}
+}*/
+
+/*char *commandAfterA() {
 	int i=0;
 	char *inFile=NULL;
 	while (every_word[i]!=NULL) {
@@ -71,94 +108,56 @@ char *file() {
 		i++;
 	}
 	return(inFile);
-}
+}*/
 
-int checkPipe(char *buffer) {
-  int i = 0;
-  int nbrOfPipe = 0;
-  char* token;
-
-  token = strtok(buffer, " ");
-  while(token != NULL) {
-    if (strcmp(token,"|") == 0) {
-      indexOfPipe[nbrOfPipe] = i;
-      nbrOfPipe++;
+int pipeRedirect(char*** nextCommand, int buffin, int buffout) {
+  pid_t pid = fork ();
+  if (pid == 0) {
+    if (buffout != 1) {
+      dup2 (buffout, 1);
+      close (buffout);
     }
-    token = strtok(NULL, " ");
-    i++;
+    if (buffin != 0) {
+      dup2 (buffin, 0);
+      close (buffin);
+    }
+    return execvp(nextCommand[0], (char * const *) nextCommand);
   }
-   return nbrOfPipe;
+  return pid;
 }
 
-void pipe_redirect(int oldfd, int newfd) {
-  if (oldfd != newfd) {
-    if (dup2(oldfd,newfd) != -1) {
-      close(oldfd);
-    } else {
-      printf("Error in duping\n");
-    }
+void execPipe(int line) {
+  pid_t pid;
+
+  int pipefd[2], k, buffin;
+  buffin = 0;
+
+  for(k=0; k < nbrOfCommands[line] - 1; k++) {
+    pipe(pipefd);
+    pipeRedirect(commands[line] + k, buffin, pipefd[1]);
+    close(pipefd[1]);
+    buffin = pipefd[0];
   }
-}
 
-void execPipe(int pos, int in_fd) {
-  if (commands[pos + 1] == NULL) {
-    pipe_redirect(in_fd, STDIN_FILENO);
-    // A MODIFIER POUR COMMAND RAPH
-    execvp(commands[pos][0], commands[pos]);
-    printf("Can't execute\n");
-    exit(0);
-  } else {
-    int pipefd[2];
-    if (pipe(pipefd) == -1) {
-      printf("Error in piping\n");
-    }
-
-    switch(fork()) {
-      case -1:
-        printf("Error in forking\n");
-      case 0:
-        child = 1;
-        close(pipefd[0]);
-        pipe_redirect(in_fd, STDIN_FILENO);
-        pipe_redirect(pipefd[1], STDOUT_FILENO);
-        //A MODIFIER POUR COMMAND RAPH
-        execvp(commands[pos][0], commands[pos]);
-        printf("Can't execute\n");
-        exit(0);
-      default:
-        close(pipefd[1]);
-        close(in_fd);
-        execPipe(pos + 1, pipefd[0]);
-    }
+  if (buffin != 0) {
+    dup2(buffin, 0);
   }
+
+  execvp(commands[line][k][0], (char * const *)commands[line][k]);
 }
 
-void execRedirect(char *fichier) {
-//    char *fichier = file();
-	pid_t pid;
-    pid = fork();
-    //Cas des erreurs
-    if(pid < 0){
-      return;
-    }
-    int redirect = (int)freopen(fichier, "w", stdout);
-    if (redirect == -1) {
-    	printf("Error file\n");
-    	exit(0);
-    }
-
-
-}
-
-void execPtVirgule() {
-	//printf("%s\n",*every_word);
-	//char *cmd1 = strdup(every_word);
-	//printf("%s\n",every_word);
-	char *cmd2 = strtok(every_word,";");
-	while (cmd2!=NULL) {
-		execPrgm(cmd2[0],cmd2);
-		cmd2=strtok(NULL,";");
-	}
+/*void execRedirect(char *fichier) {
+  pid_t pid;
+  pid = fork();
+  //Cas des erreurs
+  if(pid < 0){
+    return;
+  }
+  int redirect = (int)freopen(fichier, "w", stdout);
+  if (redirect == -1) {
+  	printf("Error file\n");
+  	exit(0);
+  }
 }
 
 void execAnd() {
@@ -199,7 +198,7 @@ void execPWD() {
   char tmpDir[128];
   getcwd(tmpDir, sizeof(tmpDir));
   printf("%s\n",tmpDir);
-}
+}*/
 
 void displayUserPath() {
   int len = strlen(getenv("HOME"));
@@ -211,84 +210,77 @@ void displayUserPath() {
 }
 
 void clearTab() {
-  int i;
-  for (i=0;i<MAX_ARGS;i++) {
-    every_word[i] = (char*) NULL;
-  }
-  for (i=0;i<MAX_PIPES;i++) {
-    indexOfPipe[i] = (int) NULL;
-  }
-  //nbrOfWord = 0;
-  //currentPipe = 0;
-}
-
-void clearTab2() {
-  int i; int j;
-  for (i = 0; i < MAX_PIPES; i++) {
-    for (j = 0; j < MAX_ARGS; j++) {
-      commands[i][j] = NULL;
+  int i; int j; int k;
+  for (k = 0; k < MAX_SEMICOLON; k++) {
+    for (i = 0; i < MAX_PIPES; i++) {
+      for (j = 0; j < MAX_ARGS; j++) {
+        commands[k][i][j] = NULL;
+      }
+      indexOfPipe[k][i] = 0;
     }
   }
+}
+
+char* copyBuffer(char *buffer) {
+  char *copy = malloc(strlen(buffer)+1);
+  strcpy(copy, buffer);
+  return copy;
 }
 
 void inputParser(char *buffer) {
   const char *delimiter = " ";
   char *token;
-  int spaceOffset = preprocessing(buffer);
-  int i;
-  char *buff = buffer + spaceOffset;
+  int line = 0;
+  int nbrOfPipe;
 
-  token = strtok(buff, delimiter);
-  //nbrOfWord = 0;
-  while(token != NULL) {
-    every_word[i] = malloc(sizeof(token));
-    strcpy(every_word[i],token);
-    token = strtok(NULL, delimiter);
-    i++;
-    //nbrOfWord++;
-  }
-}
-
-void inputParser2(char *buffer) {
-  const char *delimiter = " ";
-  char *token;
-  int spaceOffset = preprocessing(buffer);
-  char *buff = buffer + spaceOffset;
-  int limit = strlen(buff);
-  char *buff2 = malloc(strlen(buff)+1);
-  strcpy(buff2, buff);
-  int nbrOfPipe = checkPipe(buff2);
-
-  int index = 0;
-  int index2 = 0;
-  int nextPipe = 0;
-  int pos = 0;
-
+  int index;
+  int index2;
+  int nextPipe;
+  int pos;
   int indexPipe;
-  if (nbrOfPipe == 0) {
-    indexPipe = limit;
-  } else {
-    indexPipe = indexOfPipe[nextPipe];
-  }
 
-  token = strtok(buff, delimiter);
-  while(token != NULL) {
-    if (index < indexPipe) {
-      commands[pos][index2] = malloc(sizeof(token));
-      strcpy(commands[pos][index2],token);
-      index2++;
+
+  int spaceOffset = preprocessing(buffer);
+  char *unspacedBuffer = buffer + spaceOffset;
+  int limit = strlen(unspacedBuffer);
+
+  splitBySemicolon(copyBuffer(unspacedBuffer));
+  checkSemicolon(copyBuffer(unspacedBuffer));
+
+  for (line = 0; line < nbrOfLines; line++) {
+    nbrOfPipe = checkPipe(copyBuffer(lines[line]), line);
+
+    index = 0;
+    index2 = 0;
+    nextPipe = 0;
+    pos = 0;
+
+    if (nbrOfPipe == 0) {
+      indexPipe = limit;
     } else {
-      nextPipe++;
-      if (nextPipe == nbrOfPipe) {
-        indexPipe = limit;
-      } else {
-        indexPipe = indexOfPipe[nextPipe];
-      }
-      pos++;
-      index2 = 0;
+      indexPipe = (uintptr_t) indexOfPipe[line][nextPipe];
     }
-    token = strtok(NULL, delimiter);
-    index++;
+
+    token = strtok(lines[line], delimiter);
+    while(token != NULL) {
+      if (index < indexPipe) {
+        commands[line][pos][index2] = malloc(sizeof(token));
+        strcpy(commands[line][pos][index2],token);
+        index2++;
+      } else {
+        nextPipe++;
+        if (nextPipe == nbrOfPipe) {
+          indexPipe = limit;
+        } else {
+          indexPipe = (uintptr_t) indexOfPipe[line][nextPipe];
+        }
+        pos++;
+        index2 = 0;
+      }
+      token = strtok(NULL, delimiter);
+      index++;
+    }
+
   }
 
 }
@@ -304,76 +296,29 @@ int preprocessing(char *buffer) {
   return i;
 }
 
-void execPrgm(){
-    char *fichier = file();
-    char *command2 = commandAfterV();
-    char *command3 = commandAfterA();
-    pid_t pid;
-    pid = fork();
+/*void execWithPriority(int commandIndex) {
+  char *semiColon = commandAfterV(commandIndex);
 
-    //Cas des erreurs
-    if(pid < 0){
-      return;
-    }
+  if (command2 != NULL) {
+    execSemicolon(commandIndex);
+  }
 
-    if(pid==0){
+  execvp(commands[commandIndex][0], (char * const *)commands[commandIndex])
+}*/
 
-    	if (fichier != NULL) {
-  	    	execRedirect(fichier);
-  	    }
-
-  	    if (command2 != NULL) {
-  	    	execPtVirgule(every_word);
-  	    }
-
-  	    if (command3 != NULL) {
-  	    	execAnd	(every_word);
-  	    }
-      execvp(every_word[0],every_word);
-      printf("Can't execute \n");
-      exit(0);
-    }
-    else
-    {
-      WaitProcess(pid);
-    }
-}
-
-void runCommand(const char *first, char *const argv[]) {
-  int nbrOfPipe = checkPipe(argv);
-  char *fichier = file();
-  char *command2 = commandAfterV();
+void run(int line) {
   pid_t pid;
 
   pid = fork();
   if(pid < 0){
-    printf("Error during Fork\n");
+    printf("Error during Fork in : run()\n");
     return;
   }
 
   if (pid > 0) {
     WaitProcess(pid);
   } else {
-    /*if (currentPipe < nbrOfPipe) {
-      if (currentPipe == 0) {
-        printf("CAS PREMIER PIPE\n");
-        //execPipe(0, indexOfPipe[currentPipe], nbrOfWord-1);
-      } else if ((currentPipe+1) == nbrOfPipe) {
-        printf("CAS DERNIER PIPE\n");
-      } else {
-        printf("CAS PIPE\n");
-        //execPipe(indexOfPipe[currentPipe-1]+1,currentPipe, nbrOfWord-1);
-      }
-    } else if (fichier != NULL) {
-      execRedirect(fichier);
-    } else if (command2 != NULL) {
-      execPtVirgule(every_word);
-    } else {
-      execvp(first, argv);
-      printf("RunCommand : Can't execute\n");
-      exit(0);
-    }*/
-
+    execPipe(line);
   }
 }
 
@@ -384,10 +329,13 @@ int main(int argc, char *argv[]) {
 	      printf("\n");
         exit(0);
    	 }
-     clearTab2();
-     //inputParser(buffer);
-     inputParser2(buffer);
-     execPipe(0, STDIN_FILENO);
+     clearTab();
+     inputParser(buffer);
+
+     int line;
+     for(line = 0; line < nbrOfLines; line++) {
+       run(line);
+     }
 
      /*if (strcmp(every_word[0],"") != 0) { //Si Non Entrée sans rien
        if (strcmp(every_word[0],"cd") == 0) {
